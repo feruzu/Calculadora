5f11a188919c775a8dadc2e851f3d8c9d40f0ef5
feruzu <fernanda23demarzo@gmail.com>
2022-12-14 20:18:25 -0300

    commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function borrar(){
-    document.getElementById('cuenta').value='';
-    document.getElementById('resultado').value='';
+function borrar() {
+  document.getElementById("cuenta").value = "";
+  document.getElementById("resultado").value = "";
 }
 
-function btn(value){
-    document.getElementById('cuenta').value += value;
+function btn(value) {
+  document.getElementById("cuenta").value += value;
 }
 
-function calculo(){
-    var p = document.getElementById('cuenta').value;
-    var q = eval(p);
-    document.getElementById('cuenta').value = p;
-    document.getElementById('resultado').value = q;
+function calculo() {
+  var p = document.getElementById("cuenta").value;
+  var q = eval(p);
+  document.getElementById("cuenta").value = p;
+  document.getElementById("resultado").value = q;
 }
